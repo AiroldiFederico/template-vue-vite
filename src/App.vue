fdb87c37d132de24f8da9d1b4e9645c6a5a522e3
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-06 18:40:43 +0200

    Commit 1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NameComp from "./components/NameComp.vue"
+
+</script>
+
+<template>
+  <div>
+    <NameComp/>
+    <h1>Ciao Federico</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+@use "./style/main.scss" as *;
+
+
+
+</style>
---
+++ src/components/NameComp.vue
@@ -0,0 +1,43 @@
+<script>
+    
+    export default{
+
+        name: "NameComp",
+        
+        components: {
+            NameComp
+        },
+
+        data(){
+            return{
+               
+
+
+            }
+        },
+        methods: {
+
+
+        },
+    }
+    
+</script>
+
+
+
+
+<template>
+    <div id="NameComp">
+
+    </div>
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
